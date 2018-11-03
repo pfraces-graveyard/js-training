d7f08ddade77bc5bff32a56d6aac0c775ffd0c21
Pau Fracés <pfraces@gmail.com>
2018-11-03 01:16:36 +0100

    added eslint and editorconfig

--- src/sum.js
+++ src/sum.js
@@ -1,6 +1,6 @@
 function sum (x, y) {
   if (typeof x !== 'number' || typeof y !== 'number') {
-      throw new Error('non-numeric param');
+    throw new Error('non-numeric param');
   }
   
   return x + y;
